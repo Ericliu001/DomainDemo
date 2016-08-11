a14409d74733b6f536f517dcd107d99e4be666eb
Eric Liu <eric.liu.developer@gmail.com>
2016-08-11 21:29:26 +1000

    handles configuration changes

--- app/src/main/java/com/example/ericliu/domaindemo/ui/MainActivity.java
+++ app/src/main/java/com/example/ericliu/domaindemo/ui/MainActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.widget.Toolbar;
 
 import com.example.ericliu.domaindemo.R;
 import com.example.ericliu.domaindemo.adapter.PropertyListAdapter;
+import com.example.ericliu.domaindemo.util.CacheFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
     private PropertyListAdapter mAdapter;
+    private PropertyListPresenter mPropertyListPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        mAdapter = new PropertyListAdapter(new PropertyListPresenterImpl());
+        if (savedInstanceState == null) {
+            mPropertyListPresenter = new PropertyListPresenterImpl();
+            CacheFragment<PropertyListPresenter> cacheFragment = new CacheFragment<>();
+            getFragmentManager().beginTransaction().add(cacheFragment, TAG).commit();
+        } else {
+            CacheFragment<PropertyListPresenter> cacheFragment;
+            cacheFragment = (CacheFragment<PropertyListPresenter>) getFragmentManager().findFragmentByTag(TAG);
+            mPropertyListPresenter = cacheFragment.getData();
+        }
+
+        mAdapter = new PropertyListAdapter(mPropertyListPresenter);
         mRecyclerView.setAdapter(mAdapter);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
